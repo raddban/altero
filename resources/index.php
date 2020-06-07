a39e44b4115ffeef536c886595efe9a69403523d
akotikovs7 <akotikovs7@gmail.com>
2020-06-08 00:56:12 +0300

    folder with view template

---
+++ resources/index.php
@@ -0,0 +1,7 @@
+<form action="/altero" method="post">
+    <label for="e-mail">E-mail: </label>
+    <input type="email" id="e-mail" name="email" placeholder="E-mail">
+    <label for="summ">Summ: </label>
+    <input type="number" id="summ" name="summ" placeholder="0-10000">
+    <input type="submit" value="Submit" name="submit">
+</form>
